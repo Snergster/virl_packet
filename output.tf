bd25bbef0c3a035b6369cb71e73af007a795bcb6
Ed Kern <ejkern@mac.com>
2016-01-24 10:57:32 -0800

    couple test outputs

--- output.tf
+++ output.tf
@@ -4,3 +4,9 @@
     output "uwm" {
       value = "http://${packet_device.virl_test.network.0.address}:19400"
     }
+    output "uwm login" {
+      value = "login uwmadmin password ${var.uwmadmin_password}"
+    }
+    output "guest login" {
+      value = "login guest password ${var.guest_password}"
+    }
