5e9114b51e9d24caf9c787f002d407ea1efca1ac
jobstfeld <jobstfeld@hotmail.com>
2016-02-04 14:45:23 +0000

    adding ovpn reminder.

--- output.tf
+++ output.tf
@@ -10,3 +10,9 @@
     output "guest login" {
       value = "login guest password ${var.guest_password}"
     }
+    output "OpenVPN" {
+        value = "Your OpenVPN client connection file is now available at ./client.ovpn"
+    }
+    output "uwm over OpenVPN" {
+      value = "http://172.16.1.254:19400"    
+    }
